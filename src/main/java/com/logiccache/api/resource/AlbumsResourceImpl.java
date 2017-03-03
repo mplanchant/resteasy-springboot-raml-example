c30f648aee3ae9aa928e0d3c01432b2dd1ef4a77
Matthew Planchant <matthew.planchant@digital.hmrc.gov.uk>
2017-03-03 14:50:00 +0000

    First go at implementing resource methods.

--- src/main/java/com/logiccache/api/resource/AlbumsResourceImpl.java
+++ src/main/java/com/logiccache/api/resource/AlbumsResourceImpl.java
@@ -6,28 +6,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.PathParam;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class AlbumsResourceImpl implements AlbumsResource {
 
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
+    private Album blondeOnBlonde = new Album().withArtist("Bob Dylan").withReleased("1966").withTitle("Blonde on Blonde").withId(1);
+    private Album harvest = new Album().withArtist("Neil Young").withReleased("1972").withTitle("Harvest").withId(2);
+    private Album paris1919 = new Album().withArtist("John Cale").withReleased("1973").withTitle("Paris 1919").withId(3);
 
     @Override
     public GetAlbumsResponse getAlbums() throws Exception {
-        LOG.info("GET albums.");
-        return null;
+        List<Album> albumList = Arrays.asList(blondeOnBlonde, harvest, paris1919);
+        return GetAlbumsResponse.withJsonOK(outputStream -> new ObjectOutputStream(outputStream).writeObject(albumList));
     }
 
     @Override
     public PostAlbumsResponse postAlbums(Album entity) throws Exception {
         LOG.info("POST albums [{}:{}:{}].", entity.getTitle(), entity.getArtist(), entity.getReleased());
-        return null;
+        return PostAlbumsResponse.withCreated();
     }
 
     @Override
     public GetAlbumsByIdResponse getAlbumsById(@PathParam("id") Integer id) throws Exception {
         LOG.info("GET album {}.", id);
-        return null;
+        return GetAlbumsByIdResponse.withJsonOK(blondeOnBlonde);
     }
 }
